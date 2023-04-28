0897d5220b498f6df2a09256dc6d7b9aa392a700
Willian Tomaz <92401379+Willian-Tomaz@users.noreply.github.com>
2023-04-28 12:03:49 -0300

    Bug corrigido
    Adicionado a precisão necessária para realizar os cálculos

--- two.jl
+++ two.jl
@@ -1,12 +1,19 @@
 include("one.jl")
 using Base
 
-ch = ArbFloat(0.0, digits=250, base=10)
-mu = ArbFloat(0.0, digits=250, base=10)
-kp = ArbFloat(0.0, digits=250, base=10)
+setprecision(BigFloat, 250; base=10)
+
+Mp = BigFloat("250.0")
+L = BigFloat("1.0")
+rp = BigFloat("1.0")
+
+ch = big"0.0"
+mu = big"0.0"
+kp = big"0.0"
+
 
 function phi_(r)
-    phi = (rp*ch)/r^2
+    phi = big((rp*ch)/r^2)
     return phi
 end
 
@@ -17,8 +24,8 @@ end
 
 function V(r)
     phi = phi_(r)
-    g = (r^2 - rp^2) / L^2
-    return g/4 * (-(g/(4r^2)) + derivada_g(r)/2r + kp^2/r^2 + mu^2) - phi^2/4
+    g = big((r^2 - rp^2) / L^2)
+    return big(g/4 * (-(g/(4r^2)) + derivada_g(r)/2r + kp^2/r^2 + mu^2) - phi^2/4)
 end
 
 function P2a(r)
@@ -26,8 +33,8 @@ function P2a(r)
 end
 
 function fch(r)
-    res1 = (L^2 * log(Complex(1 - r/rp)) ) / ( 2 *rp )
-    res2 = ( L^2 * log(Complex(1 + r/rp)) ) / (2 * rp)
+    res1 = big((L^2 * log(Complex(1 - r/rp)) ) / ( 2 *rp ))
+    res2 = big(( L^2 * log(Complex(1 + r/rp)) ) / (2 * rp))
     res =  res1 - res2
     return real(res)
 end
@@ -41,15 +48,16 @@ for i in x
 end
 
 plot(r -> V(r), rp, 10rp, xlabel="r", ylabel="V(r)", title="Gráfico de V(r)")
-savefig("2_gráfico/grafico_4.png")
+savefig("2_gráfico/grafico_4.pdf")
+#savefig("Teste/kp_0/1.pdf")
 
-P = zeros(ArbFloat, (Int((te-ts)/h) + 2, 1))
+P = zeros(BigFloat, (Int((te-ts)/h) + 2, 1))
 P2 = zeros(ArbComplex, (Int((te-ts)/h) + 2, 1))
 
 for i in 1:div(te - ts, h) + 1
     i = convert(Int, i)     
-    P[i] = ArbFloat(V(tort[i]))
-    P2[i] = P2a(tort[i])
+    P[i] = big( V(tort[i])  )
+    P2[i] =  P2a(tort[i])
 end
 
 
@@ -57,54 +65,58 @@ save_vec(P, "P_julia")
 save_vec(P2, "P2_julia")
 
 kpp = 0
-seg = 400
+ 
 
 Fd = zeros(BigFloat, Int(div(te-ts,h))+6)
 LFd = zeros(BigFloat, Int(div(te-ts,h))+6)
+s = zeros(BigFloat, Int(div(te-ts, h)) + 6)
 
 
-s = zeros(ArbFloat, Int(div(te-ts, h)) + 6)
 @info "Comprimento s: $(length(s))"
 
-s[1] = ArbFloat(1.0, digits=250, base=10) 
+s[1] = big"1.0" 
 
 seg = div(length(s), 10)
 len = length(P)
 
+#seg = 400
  
-setprecision(BigFloat, 800)
 t = @elapsed begin
-    for i in 0: k2 + k1 - 4
-        i = floor(Int, i)
-        var = s[1]
-        s[1] = 1.0
-        
-        if i % seg == 0
-            @info "$i"
-        end
+    map(i -> begin
+            i = Int(i)
+            var = s[1]
+            s[1] = 1.0
 
-        for j in 2:i+2
-            j = floor(Int, j)
-            tmp = s[j]
-            global k1 = floor(Int, k1)
-            global k2 = floor(Int, k2)
-            p1 = P[j+k2+k1-i-3]
-            p2 = P2[j+k2+k1-i-2]
-            p3 = P[j+k2+k1-i-1]
-            
-            s[j] = ArbFloat((s[j-1] + s[j] - var * (1 - 2h*p2) - 2h^2 * (p1*s[j-1] + p3*s[j])) * ((2h*p2 + 1)^(-1)), digits=250, base=10)
-            
-            var = tmp
-            LFd[j-1] = log(s[j]^2)/2
-            Fd[j-1] = s[j]
-            
-            if j == i+2
-                global kpp += 1
+            if i % seg == 0
+                @info "$i"
             end
-        end
-    end
-end
 
+            map(j -> begin
+                    if j == i +2
+                        s[j] = 0
+                    end
+                    tmp = s[j]
+                    global k1 = floor(Int, k1)
+                    global k2 = floor(Int, k2)
+                    p1 = BigFloat(P[j+k2+k1-i-3])
+                    p2 = 0.0
+                    p3 = BigFloat(P[j+k2+k1-i-1])
+
+                    s[j] = BigFloat((s[j-1] + s[j] - var * (1 - 2h*p2) - 2h^2 * (p1*s[j-1] + p3*s[j])) * ((2h*p2 + 1)^(-1)))
+
+                    var = tmp
+ 
+                    if j == i+2
+                        LFd[j-1] = BigFloat(log(s[j]^2)/2)
+                        Fd[j-1] = s[j]
+
+                    else
+                        global kpp += 1
+                    end
+                end, 2:i+2)
+        end, 0: k2 + k1 - 4)
+end
+@info "kpp: $kpp"
  
 t_for = round(t, digits=2)
 @info "Tempo de execução (for): $t_for s"
@@ -114,10 +126,15 @@ v1 = length(LFd)
 
 plot(real(LFd[1:v1]))
 savefig("2_gráfico/grafico_5.pdf")
+#savefig("Teste/kp_0/2.pdf")
 
 
-scatter(1:v1, real(LFd[1:v1]), markersize=1)
+scatter(1:v1, real(LFd[1:v1]), marker=:circle, markersize=2, color=:black,
+        xlabel="Índice", ylabel="Valor de LFd", title="Gráfico de LFd")
+
+#savefig("Teste/kp_0/3.pdf")
 savefig("2_gráfico/grafico_6.pdf")
+
 
 
 @info "Comprimento s: $(length(s))"
@@ -125,6 +142,6 @@ savefig("2_gráfico/grafico_6.pdf")
 @info "Comprimento P2: $(length(P2))"
 
 save_vec(s, "s_julia")
-save_vec(LFd, "LFd_julia")
-save_vec(Fd, "Fd_julia")
+#save_vec(LFd, "LFd_julia")
+#save_vec(Fd, "Fd_julia")
 
